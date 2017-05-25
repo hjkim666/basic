dfae2fe686b76e87262062449814f211917f932b
hjkim666 <hjkim666@nate.com>
2017-05-25 17:05:13 +0900

    stock

---
+++ stock.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+tf.set_random_seed(777) 
+def MinMaxScaler(data):
+    num_row = np.shape(data)[0]
+    num_col = np.shape(data)[1]
+    array = np.zeros((num_row, num_col))
+    for i in range(num_col):
+        input = data[:, i]
+        array[:, i] = (input - np.min(input)) / (np.max(input) - np.min(input))
+    return array
+
+timesteps = seq_length = 7
+data_dim = 5
+output_dim = 1
+# Open,High,Low,Close,Volume
+xy = np.loadtxt('stock.csv', delimiter=',')
+xy = xy[::-1]  # reverse order (chronically ordered)
+xy = MinMaxScaler(xy)
+x = xy
+y = xy[:, [-1]]  # Close as label
+
+dataX = []
+dataY = []
+for i in range(0, len(y) - seq_length):
+   _x = x[i:i + seq_length]
+   _y = y[i + seq_length]  # Next close price
+   print(_x, "->", _y)
+   dataX.append(_x)
+   dataY.append(_y)
+
+# split to train and testing
+train_size = int(len(dataY) * 0.7)
+test_size = len(dataY) - train_size
+trainX, testX = np.array(dataX[0:train_size]),np.array(dataX[train_size:len(dataX)])
+trainY, testY = np.array(dataY[0:train_size]),np.array(dataY[train_size:len(dataY)])
+
+# input placeholders
+X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
+Y = tf.placeholder(tf.float32, [None, 1])
+
+cell = tf.contrib.rnn.BasicLSTMCell(num_units=output_dim, state_is_tuple=True)
+outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+Y_pred = outputs[:, -1]  # We use the last cell's output
+
+# cost/loss
+loss = tf.reduce_sum(tf.square(Y_pred - Y))  # sum of the squares
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for i in range(1000):
+   _, l = sess.run([train, loss], 
+          feed_dict={X: trainX, Y: trainY})
+   print(i, l)
+
+testPredict = sess.run(Y_pred, feed_dict={X: testX})
+
+import matplotlib.pyplot as plt
+plt.plot(testY)
+plt.plot(testPredict)
+plt.show()

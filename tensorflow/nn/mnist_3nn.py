1f95fb69bd81d44ccd0bdd95e7fda1703a36d4f0
hjkim666 <hjkim666@nate.com>
2018-02-22 16:44:12 +0900

    mnist

--- tensorflow/nn/mnist_3nn.py
+++ tensorflow/nn/mnist_3nn.py
@@ -47,7 +47,7 @@ print(hypothesis.eval({X: mnist.test.images, Y: mnist.test.labels, dropout_rate:
 
 r = random.randint(0, mnist.test.num_examples -1)
 print('Label:', sess.run(tf.argmax(mnist.test.labels[r:r+1],1)))
-print('Prediction:', sess.run(tf.argmax(hypothesis,1),{X:mnist.test.images[r:r+1]}))
+print('Prediction:', sess.run(tf.argmax(hypothesis,1),{X:mnist.test.images[r:r+1], dropout_rate:1}))
  
 plt.imshow(mnist.test.images[r:r+1].reshape(28,28)
            , cmap='Greys', interpolation='nearest')

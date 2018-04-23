8d7a5021678d973b1e13f314a78ae641e077bc73
hjkim666 <hjkim666@nate.com>
2018-04-23 12:18:59 +0900

    source

---
+++ core/dic_test.py
@@ -0,0 +1,35 @@
+people = {
+      'Kim':{
+            'phone':'1234',
+            'addr':'seoul'
+            },
+      'Lee':{
+            'phone':'5678',
+            'addr':'pusan'
+            },
+      'Park':{
+            'phone':'9090',
+            'addr':'guro'
+            },
+      }
+
+# Descriptive lebels for the phone number and address. These will be used
+# when printing the output
+
+labels = {
+      'phone':'phone number',
+      'addr':'address'
+      }
+name = input('Name: ')
+request = input('Phone number (p) or address (a)? ')
+
+key = request
+if request == 'p': key='phone'
+if request == 'a': key='addr'
+
+person = people.get(name,{})
+label=labels.get(key, key)
+result=person.get(key,'not available')
+
+print("%s's %s is %s." % (name, label, result))
+      
